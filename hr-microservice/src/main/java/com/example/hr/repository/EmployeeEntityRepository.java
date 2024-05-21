586e23f97f4dc2bd115837c7dc3a1f3541c7d7d3
Binnur KURT <deepcloudlabs@gmail.com>
2024-05-21 15:35:22 +0300

    Update EmployeeEntityRepository.java

--- hr-microservice/src/main/java/com/example/hr/repository/EmployeeEntityRepository.java
+++ hr-microservice/src/main/java/com/example/hr/repository/EmployeeEntityRepository.java
@@ -6,21 +6,20 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.example.hr.domain.Employee;
 import com.example.hr.entity.EmployeeEntity;
 
 public interface EmployeeEntityRepository extends JpaRepository<EmployeeEntity, String>{
-	List<Employee> getAllByBirthYearBetween(int beginYear,int endYear);
+	List<EmployeeEntity> getAllByBirthYearBetween(int beginYear,int endYear);
 	
 	// JPQL
 	@Query(value = "select e from EmployeeEntity e where e.birthYear between :beginYear and :endYear", nativeQuery = false)
-	List<Employee> yasAraliginaGoreGetir(int beginYear,int endYear);
+	List<EmployeeEntity> yasAraliginaGoreGetir(int beginYear,int endYear);
 	
 	// Native SQL
 	@Query(value = "select e from employees e where e.byear between :beginYear and :endYear", nativeQuery = true)
-	List<Employee> bulGetir(int beginYear,int endYear);
+	List<EmployeeEntity> bulGetir(int beginYear,int endYear);
 	
-	Collection<Employee> findFirst10ByLastName(String lastname);
-	Employee findTopByOrderByBirthYearDesc();
-	Employee findTopByOrderBySalaryDesc();
+	Collection<EmployeeEntity> findFirst10ByLastName(String lastname);
+	EmployeeEntity findTopByOrderByBirthYearDesc();
+	EmployeeEntity findTopByOrderBySalaryDesc();
 }
